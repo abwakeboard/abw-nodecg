37ce95c9736bc866d1c511350217085eaeaa00da
saadbruno <saadbruno@gmail.com>
2026-07-26 03:04:55 -0300

    Remove data de nascimento da query do GraphQL
    A API publica do LiveHeats não dá mais essa informação. Pra pegar essa info, precisa de login

--- extension/modules/liveheats.js
+++ extension/modules/liveheats.js
@@ -54,7 +54,6 @@ async function fetchOrdemEntrada(idLiveHeats) {
                                                 name
                                                 id
                                                 image
-                                                dob
                                             }
                                         }
                                         eventDivision {
